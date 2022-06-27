2e46188eb7626b4224f341ab0579beba52ac151d
karihold <45006052+karihold@users.noreply.github.com>
2022-06-27 17:18:23 +0200

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createRoot } from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Frontpage from "pages/frontpage/frontpage";
 import Header from "landmarks/header/header";
 import Footer from "landmarks/footer/footer";
 import "scss/base.scss";
 
+const root = createRoot(document.querySelector("#render-target"));
+
 const App = () => {
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <Frontpage />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route index element={<Frontpage />} />
+      </Routes>
       <Footer />
     </Router>
   );
 };
 
-render(<App />, document.querySelector("#render-target"));
+root.render(<App />);
